975b5165f5c8d3e267e9263c6d85ca8f957533c3
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-09-12 20:54:48 -0400

    Add project files.

---
+++ JobBoardCOMP2084LU1206780/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobBoardCOMP2084LU1206780.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
