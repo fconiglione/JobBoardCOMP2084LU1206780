001512d96bcdb3cde62b00d07a1bd0f00f93fd3e
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-28 23:10:18 -0500

    Setup the unit testing project

---
+++ JobBoardCOMP2084LU1206780Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JobBoardCOMP2084LU1206780Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JobBoardCOMP2084LU1206780Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0b1c220-fb27-414f-aacd-0c2723200fec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JobBoardCOMP2084LU1206780Tests/CompaniesControllerTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoardCOMP2084LU1206780.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobBoardCOMP2084LU1206780.Data;
+using Microsoft.EntityFrameworkCore;
+using JobBoardCOMP2084LU1206780.Models;
+
+namespace JobBoardCOMP2084LU1206780Tests
+{
+    [TestClass]
+    public class CompaniesControllerTest
+    {
+        ApplicationDbContext _context;
+        CompaniesController controller;
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
