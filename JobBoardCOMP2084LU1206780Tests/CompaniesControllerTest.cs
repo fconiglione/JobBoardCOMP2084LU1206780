5b63cc0882b359e82c714eb659a3224d84041a9b
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-29 15:12:47 -0500

    created 4 get test methods and 2 post test methods for the "Delete" companies functionality

--- JobBoardCOMP2084LU1206780Tests/CompaniesControllerTest.cs
+++ JobBoardCOMP2084LU1206780Tests/CompaniesControllerTest.cs
@@ -17,6 +17,7 @@ namespace JobBoardCOMP2084LU1206780Tests
         ApplicationDbContext _context;
         CompaniesController controller;
 
+        // Initializing tests with mock data
         [TestInitialize]
         public void TestInitialize()
         {
@@ -38,12 +39,115 @@ namespace JobBoardCOMP2084LU1206780Tests
 
             controller = new CompaniesController(_context);
         }
+
+        // Delete [GET] Test Methods
+        // Note: I could not figure out how to test for user authorization otherwise I would have included this in the test methods
+
+        // Testing Delete() with null id
         [TestMethod]
-        public void IndexReturnsView()
+        public async Task DeleteNullIdReturnsNotFound()
         {
-            var results = (ViewResult)controller.Index().Result;
-            Assert.AreEqual("Index", results.ViewName);
+            // Act
+            var result = await controller.Delete(null);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            // Ensuring NotFound() is returned
+            var notFoundResult = result as NotFoundResult;
+            Assert.IsNotNull(notFoundResult);
         }
 
+        // Testing Delete() with valid id
+        [TestMethod]
+        public async Task DeleteValidIdReturnsNotFound()
+        {
+            // Act
+            var result = await controller.Delete(71);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+
+            // Ensure item is properly deleted
+            Assert.AreEqual("Delete", viewResult.ViewName);
+        }
+
+        // Testing Delete() with invalid id
+        [TestMethod]
+        public async Task DeleteInvalidIdReturnsNotFound()
+        {
+            // Act
+            var result = await controller.Delete(97);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            // Ensuring NotFound() is returned
+            var notFoundResult = result as NotFoundResult;
+            Assert.IsNotNull(notFoundResult);
+        }
+
+        [TestMethod]
+        public async Task DeleteValidIdReturnsCorrectCompany()
+        {
+            // Act : using a valid CompanyId
+            var result = await controller.Details(71);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+
+            var model = viewResult.Model as Company;
+            Assert.IsNotNull(model);
+
+            // Compare company IDs; ensure correct company is deleted
+            Assert.AreEqual(71, model.CompanyId);
+        }
+
+        // Delete [POST] Test Methods
+        // Note: I would have liked to have made one more test method to test for null company context
+
+        // Testing DeleteConfirmed() with valid CompanyId
+        [TestMethod]
+        public async Task DeleteCompanyWithValidIdFromDb()
+        {
+            // Act : using a valid CompanyId
+            var result = await controller.DeleteConfirmed(191);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            var redirectToActionResult = result as RedirectToActionResult;
+            Assert.IsNotNull(redirectToActionResult);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+
+            // Check if the company was deleted from the database
+            var deletedCompany = await _context.Companies.FindAsync(191);
+            Assert.IsNull(deletedCompany);
+        }
+
+        // Testing DeleteConfirmed() with invalid ID
+        [TestMethod]
+        public async Task DeleteCompanyWithInvalidIdFromDb()
+        {
+            // Act : using an invalid CompanyId
+            var result = await controller.DeleteConfirmed(999);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            var redirectToActionResult = result as RedirectToActionResult;
+            Assert.IsNotNull(redirectToActionResult);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+
+            // Ensure that no company was deleted from the database
+            var notDeletedCompany = await _context.Companies.FindAsync(999);
+            Assert.IsNull(notDeletedCompany);
+        }
     }
 }
